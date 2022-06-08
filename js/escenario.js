5912d08334e26acaa3f41334fd07f6eabb9fb76c
Gonzalo Cozzo <gonza007_88@hotmail.com>
2022-06-08 18:51:38 -0300

    probando errores github pages

--- js/escenario.js
+++ js/escenario.js
@@ -22,9 +22,9 @@ var errores = 0;
 
 /* Palabras */
 
-/* var palabras_array = [ "GATO","PERRO","LEON","CABALLO","BALLENA","TIBURON","ELEFANTE","TUCAN","ERIZO","CONEJO","TIGRE","CONDOR","ZEBRA","COCODRILO"];
+ var palabras = [ "GATO","PERRO","LEON","CABALLO","BALLENA","TIBURON","ELEFANTE","TUCAN","ERIZO","CONEJO","TIGRE","CONDOR","ZEBRA","COCODRILO"];
        
- localStorage.setItem('myArray', JSON.stringify(palabras_array)); 
+/* localStorage.setItem('myArray', JSON.stringify(palabras_array)); 
 
  function llamardatosstg() {
      var palabranueva = sessionStorage.getItem("palabraNueva");
