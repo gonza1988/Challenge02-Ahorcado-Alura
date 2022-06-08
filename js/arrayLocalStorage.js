c4fefadd7b31c19451a58ada33b25441dee4bfa8
Gonzalo Cozzo <gonza007_88@hotmail.com>
2022-06-08 18:44:56 -0300

    probando arreglos a github pages

--- js/arrayLocalStorage.js
+++ js/arrayLocalStorage.js
@@ -1,3 +1,3 @@
-var palabras_array = ["GATO", "PERRO", "LEON", "CABALLO", "BALLENA", "TIBURON", "ELEFANTE", "TUCAN", "ERIZO", "CONEJO", "TIGRE", "CONDOR", "ZEBRA", "COCODRILO"];
+var palabras = ["GATO", "PERRO", "LEON", "CABALLO", "BALLENA", "TIBURON", "ELEFANTE", "TUCAN", "ERIZO", "CONEJO", "TIGRE", "CONDOR", "ZEBRA", "COCODRILO"];
 
-localStorage.setItem('myArray', JSON.stringify(palabras_array));
+localStorage.setItem('myArray', JSON.stringify(palabras));
